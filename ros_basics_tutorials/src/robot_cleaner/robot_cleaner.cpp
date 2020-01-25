886e339b303c3aba889fea69b0e675f1c10214d4
AMIN <modamine@gmail.com>
2020-01-25 23:55:58 +0100

    changed the formula for caluclating distance_moved from srqrt(x_2 +y_2) to velocity * time passed and added a function that rotated the robot following the same algorithm but with angles

--- ros_basics_tutorials/src/robot_cleaner/robot_cleaner.cpp
+++ ros_basics_tutorials/src/robot_cleaner/robot_cleaner.cpp
@@ -1,40 +1,97 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <turtlesim/Pose.h>
-#include <math>
+
 
 double x,y,yaw;
 ros::Publisher velocity_publisher ;
+ros::Subscriber pose_subscriber ;
+turtlesim::Pose t_p;
+const double PI = 3.14159265359;
 
-void poseCallback(const turtlesim::Pose::constPtr& pose_msg)
+void poseCallback(const turtlesim::Pose::ConstPtr& pose_msg)
 {
-x = pose_msg.x;
-y = pose_msg.y;
-yaw = pose_msg.theta;
+t_p.x = pose_msg->x;
+t_p.y = pose_msg->y;
+t_p.theta = pose_msg->theta;
+}
+
+double getDistance(double x1, double y1, double x2, double y2){
+	return sqrt(pow((x1-x2),2)+pow((y1-y2),2));
+}
+
+double degrees2radians(double angle_in_degrees){
+	return angle_in_degrees *PI /180.0;
 }
 
 void move(float speed ,float distance , bool isForward)
 {
-double x0=x,y0=y,yaw0=yaw;
+// variable declaration    
 float distance_moved = 0.0 ;
 geometry_msgs::Twist velocity_message;
-if(is_forward)
+ros::Rate loop_rate(10);
+
+//speed setting
+if(isForward)
     velocity_message.linear.x = abs(speed);
 else 
     velocity_message.linear.x = -abs(speed);
-}
-ros::rate loop_rate(10);
-velocity_publisher =  n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel",10);
-while (distance_moved < distance)
+double t0 = ros::Time::now().toSec();
+
+//keep sendig velocity messages while u've not reached the required distance
+do
 {
-    ROS_INFO("turtle_sim moves forward");
     velocity_publisher.publish(velocity_message);
-    distance_moved = abs(0.5*sqrt(pow((x-x0),2)+pow((y-y0),2)));
-    ROS_INFO("distance walked = %f",distance_moved);
+    double t1 = ros::Time::now().toSec();
+    distance_moved = speed*(t1-t0);
     ros::spinOnce(); 
     loop_rate.sleep();
-}
+}while (distance_moved < distance);
+
+//tell the robot to stop distance reached 
 velocity_message.linear.x = 0;
+velocity_publisher.publish(velocity_message);
+ros::spinOnce(); 
+loop_rate.sleep();
+}
+
+void rotate(float angular_speed ,float angle , bool isCloackwise)
+{
+//variables declarations
+geometry_msgs::Twist velocity_message;
+float angle_moved = 0;
+ros::Rate loop_rate(10);
+
+//resetting all velocities
+velocity_message.linear.x = 0;
+velocity_message.linear.y = 0;
+velocity_message.linear.z = 0;
+velocity_message.angular.x = 0;
+velocity_message.angular.y = 0;
+velocity_message.angular.z = 0;
+
+//velocity settings 
+if (isCloackwise)
+{
+    velocity_message.angular.z = -angular_speed;
+}else
+{
+    velocity_message.angular.z = angular_speed;
+}
+
+//keep sendig velocity messages while u've not reached the required angle
+double t0 = ros::Time::now().toSec();
+do 
+{
+    velocity_publisher.publish(velocity_message);
+    double t1 = ros::Time::now().toSec();
+    angle_moved = (t1-t0) * angular_speed ;
+    ros::spinOnce(); 
+    loop_rate.sleep();
+}while(angle_moved < angle);
+
+//tell the robot to stop turning 
+velocity_message.angular.z = 0;
 velocity_publisher.publish(velocity_message);
 ros::spinOnce(); 
 loop_rate.sleep();
@@ -49,7 +106,8 @@ int main(int argc, char **argv)
     // Initiate a new ROS node named "listener"
 	ros::init(argc, argv, "robot_cleaner");
 	//create a node handle: it is reference assigned to a new node
-	ros::NodeHandle node;
-    ros::Subscriber pose_subscriber = n.Subscribe("/turtle1/pose",1000,poseCallback);
+	ros::NodeHandle n;
+    pose_subscriber = n.subscribe("/turtle1/pose",1000,poseCallback);
+    velocity_publisher =  n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel",10);
     move(10,10,true);
 }
